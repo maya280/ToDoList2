0f900ee4df95c96c72454d22283b8d22d8b456af
Maya <mayaelee28@gmail.com>
2021-09-14 22:25:47 -0400

    needed to delete all of weather attempt. unfortunately. commented out what i could but it interfered with the build, so its gone.

--- ToDoList2/WeatherController.swift
+++ ToDoList2/WeatherController.swift
@@ -9,28 +9,28 @@ import UIKit
 import CoreLocation
 
 //creates the struct for the weather API
-public struct Forecast: Codable {
-    struct Daily: Codable {
-        let dt: Date
-        struct Temp: Codable {
-            let min: Double
-            let max: Double
-        }
-        let temp: Temp
-        let humidity: Int
-        struct Weather: Codable {
-            let id: Int
-            let description: String
-            let icon: String
-            var weatherIconURL: URL {
-                let urlString = "ttp://openweathermap.org/img/wn/\(icon)@2x.png"
-                return URL(string: urlString)!
-            }
-        }
-        let weather: [Weather]
-        let clouds: Int
-    }
-}
+//public struct Forecast: Codable {
+//    struct Daily: Codable {
+//        let dt: Date
+//        struct Temp: Codable {
+//            let min: Double
+//            let max: Double
+//        }
+//        let temp: Temp
+//        let humidity: Int
+//        struct Weather: Codable {
+//            let id: Int
+//            let description: String
+//            let icon: String
+//            var weatherIconURL: URL {
+//                let urlString = "ttp://openweathermap.org/img/wn/\(icon)@2x.png"
+//                return URL(string: urlString)!
+//            }
+//        }
+//        let weather: [Weather]
+//        let clouds: Int
+//    }
+//}
 
 //how to find the lat and long from city name
 //CLGeocoder().geocodeAddresssString("Vancouver") { (placemarks, error) in
@@ -58,19 +58,19 @@ class WeatherController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func getWeather(_ sender: Any) {
-        let zipStr: String = zip.text!
-        print(zipStr)
-//        let apiService = APIWeatherService.shared
-        let geoCoder = CLGeocoder()
-        var lat: CLLocationDegrees = 0
-        var lon: CLLocationDegrees = 0
-        geoCoder.geocodeAddressString(zipStr) { (placemarks, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            }
-            lat = placemarks?.first?.location?.coordinate.latitude ?? 0
-            lon = placemarks?.first?.location?.coordinate.longitude ?? 0
+//    @IBAction func getWeather(_ sender: Any) {
+//        let zipStr: String = zip.text!
+//        print(zipStr)
+////        let apiService = APIWeatherService.shared
+//        let geoCoder = CLGeocoder()
+//        var lat: CLLocationDegrees = 0
+//        var lon: CLLocationDegrees = 0
+//        geoCoder.geocodeAddressString(zipStr) { (placemarks, error) in
+//            if let error = error {
+//                print(error.localizedDescription)
+//            }
+//            lat = placemarks?.first?.location?.coordinate.latitude ?? 0
+//            lon = placemarks?.first?.location?.coordinate.longitude ?? 0
             
             //            if
 //                let lat = placemarks.first?.location?.coordinate?.latitude
@@ -83,31 +83,31 @@ class WeatherController: UIViewController {
 //                self.tempDisplay.text = "Invalid zipcode"
 //            }
         }
-        print(lat)
-        print(lon)
+//        print(lat)
+//        print(lon)
         
-        if let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=current,minutely,hourly,alerts&appid=c9c3bf612706e65b527065820b52c899") {
-            URLSession.shared.dataTask(with: url) { [self]
-                weatherdata, respose, error in
-                if let weatherdata = weatherdata {
-                    if let jsonString = String(data: weatherdata, encoding: .utf8) {
-                        //print(jsonString)
-                        let res = jsonString.components(separatedBy: "temp")
-                        let finaltempKelvinStr = (((res[1].components(separatedBy: ":"))[1].components(separatedBy: ","))[0])
-                        let tempKelvinDoub = Double(finaltempKelvinStr)
-                        //convert from Kelvin to F
-                        let finalTempF = (((tempKelvinDoub! - 273.15)*Double(9))/Double(5)) + Double(32)
-                        let tempDisplay = String(format: "%.1f", finalTempF)
-                        print("actual temp \(tempDisplay)")
-                        DispatchQueue.main.async {
-                            print("changing now")
-                            self.tempDisplay.text = "\(tempDisplay) *F"
-                                   }
-                        
-                    }
-                }
-            }.resume()
-        }
+//        if let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=current,minutely,hourly,alerts&appid=c9c3bf612706e65b527065820b52c899") {
+//            URLSession.shared.dataTask(with: url) { [self]
+//                weatherdata, respose, error in
+//                if let weatherdata = weatherdata {
+//                    if let jsonString = String(data: weatherdata, encoding: .utf8) {
+//                        //print(jsonString)
+//                        let res = jsonString.components(separatedBy: "temp")
+//                        let finaltempKelvinStr = (((res[1].components(separatedBy: ":"))[1].components(separatedBy: ","))[0])
+//                        let tempKelvinDoub = Double(finaltempKelvinStr)
+//                        //convert from Kelvin to F
+//                        //let finalTempF = (((tempKelvinDoub! - 273.15)*Double(9))/Double(5)) + Double(32)
+//                        let tempDisplay = String(format: "%.1f", finalTempF)
+//                        print("actual temp \(tempDisplay)")
+//                        DispatchQueue.main.async {
+//                            print("changing now")
+//                            self.tempDisplay.text = "\(tempDisplay) *F"
+//                                   }
+//
+//                    }
+//                }
+//            }.resume()
+//        }
         
 //        if let lat = placemarks?.first?.location?.coordinate.latitude,
 //           let lon = placemarks?.first?.location?.coordinate.longitude {
@@ -135,6 +135,6 @@ class WeatherController: UIViewController {
 //            }
 //        }
     
-    }
-    
-}
+//    }
+//
+//}
